7242c1685233ee34418c2c9fb9570fad12442f08
Yujaw Manandhar <yujawmanandhar@gmail.com>
2023-10-13 08:58:28 +0545

    Users info update added

--- src/components/Account/accountInfo.jsx
+++ src/components/Account/accountInfo.jsx
@@ -1,14 +1,38 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import useAxiosPrivate from '../../hooks/useAxiosPrivate'
+import useAuth from '../../hooks/useAuth'
 
 const AccountInfo = () => {
 
-    const [user, setUser] = useState({
-        fname: '',
-        lname: '',
-        address: '',
-        postalcode: '',
-        city: '',
-    })
+    const [user, setUser] = useState({})
+
+    const axiosPrivate = useAxiosPrivate()
+    const { auth } = useAuth()
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                await axiosPrivate
+                    .get(`/api/users/${auth?.email}`)
+                    .then((res) => {
+                        setUser(res.data.data)
+                    })
+            } catch (err) {
+                console.log(err)
+            }
+        }
+        fetchData()
+    }, [auth?.email, axiosPrivate])
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        try {
+            await axiosPrivate
+                .post(`/api/users/${auth?.email}`, { user })
+        } catch (err) {
+            console.log(err)
+        }
+    }
 
     return (
         <>
@@ -24,7 +48,7 @@ const AccountInfo = () => {
                 <div className='container'>
                     <div className='sub_container'>
                         <div className='title'>
-                            Billed to
+                            Account
                         </div>
                         <div className='items'>
                             <input type='text' placeholder='First Name' value={user.fname} onChange={e => {
@@ -38,20 +62,20 @@ const AccountInfo = () => {
                             <input type='text' placeholder='Address' value={user.address} onChange={e => {
                                 setUser((currItems) => { return { ...currItems, address: e.target.value } })
                             }} />
-                            <input type='number' placeholder='Zip' value={user.postalcode} onChange={e => {
-                                setUser((currItems) => { return { ...currItems, postalcode: e.target.value } })
+                            <input type='text' placeholder='City' value={user.city} onChange={e => {
+                                setUser((currItems) => { return { ...currItems, city: e.target.value } })
                             }} />
                         </div>
                         <div className='items'>
-                            <input type='text' placeholder='City' value={user.city} onChange={e => {
-                                setUser((currItems) => { return { ...currItems, city: e.target.value } })
+                            <input type='number' placeholder='Zip' value={user.postalcode} onChange={e => {
+                                setUser((currItems) => { return { ...currItems, postalcode: e.target.value } })
                             }} />
                         </div>
                     </div>
                 </div>
                 <div className='button'>
                     <button className='secondary'>Reset</button>
-                    <button className='primary'>Save</button>
+                    <button className='primary' onClick={handleSubmit}>Save</button>
                 </div>
             </div>
         </>
