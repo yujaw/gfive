727e37b6addf20b3308b73a55163314f9aaa805e
Yujaw Manandhar <yujawmanandhar@gmail.com>
2024-12-04 11:26:53 +0545

    removed path

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -28,7 +28,7 @@ const Routes = () => {
             element: <PersistLogin />,
             children: [
                 {
-                    path: '/gfive',
+                    path: '/',
                     element: <Layouts />,
                     children: [
                         {
